b71144c76939cb42d914363127e8f7e310b633f3
SubhashPrasad23 <SubhashPrasad027@gmail.com>
2026-04-23 00:41:50 +0530

    remove env from repo and add gitignore

--- server/app.js
+++ server/app.js
@@ -4,7 +4,22 @@ import generateRoute from "./routes/generate.js";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+    "http://localhost:5173", 
+    "https://promo-script-generator.vercel.app"
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(null, false);
+        }
+    },
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use("/generate-video", generateRoute);
@@ -13,6 +28,8 @@ app.get("/", (req, res) => {
     res.send("Backend running ");
 });
 
-app.listen(5000, () => {
-    console.log("Server running on 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
